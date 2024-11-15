0a97633c7e24aedea7a9717c83a69906b0fa4949
Mateo Espinoza Monti <132113148+CTMDOVZ@users.noreply.github.com>
2024-11-14 19:30:26 -0500

    Update auth.py

--- usuarios-api/src/utils/auth.py
+++ usuarios-api/src/utils/auth.py
@@ -5,12 +5,14 @@ from datetime import datetime, timedelta
 SECRET_KEY = os.environ['JWT_SECRET']
 
 def generate_token(data):
+    """
+    Genera un token JWT que incluye user_id en el payload.
+    """
     expiration = datetime.utcnow() + timedelta(hours=1)
-    data.update({"exp": expiration})
-    return jwt.encode(data, SECRET_KEY, algorithm="HS256")
-
-def verify_token(token):
-    try:
-        return jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return None
+   
+    payload = {
+        "user_id": data["id_usuario"],  
+        "tenant_id": data["tenant_id"],
+        "exp": expiration
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
